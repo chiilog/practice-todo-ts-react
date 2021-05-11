760eb4e058a50cfee91927ab0ad60984bf38781e
Chiaki Obara <obara@ham.works>
2021-05-11 19:57:42 +0900

    setup ChakraProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "./theme/theme";
 
 export const App = () => {
-	return <div></div>;
+	return <ChakraProvider theme={theme}></ChakraProvider>;
 };
