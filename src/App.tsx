399a562e1d6f8eb52cfe33ccc9a1b0344f137bd3
Chiaki Obara <obara@ham.works>
2021-05-12 20:06:53 +0900

    onClickの追加（アラートのみ）

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,22 @@ import { Header } from "./components/organisms/Header";
 import { TodoItem } from "./components/molecules/TodoItem";
 
 export const App = () => {
+	const onClickComplete = () => alert("completed!");
+	const onClickDelete = () => alert("deleted!");
+
 	return (
 		<ChakraProvider theme={theme}>
 			<Header />
 			<Box as="p" m={4} fontSize="sm">
-				ダブルクリックで項目を編集できます
+				テキストをダブルクリックすると項目を編集できます
 			</Box>
 			<List my={4} bg="white" boxShadow="md">
 				{[...Array(10)].map((res, index) => (
-					<TodoItem key={index} />
+					<TodoItem
+						key={index}
+						onClickComplete={onClickComplete}
+						onClickDelete={onClickDelete}
+					/>
 				))}
 			</List>
 		</ChakraProvider>
--- src/components/molecules/TodoItem.tsx
+++ src/components/molecules/TodoItem.tsx
@@ -1,22 +1,40 @@
-import { Flex, IconButton, Input, ListItem } from "@chakra-ui/react";
-import { CheckCircleIcon } from "@chakra-ui/icons";
+import { Box, Flex, IconButton, Input, ListItem } from "@chakra-ui/react";
+import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 import React, { memo, VFC } from "react";
+
+type Props = {
+	onClickComplete: () => void;
+	onClickDelete: () => void;
+};
 
 /**
  * TODO: 完了の場合文字色は薄いグレー（gray.300）でチェックマークは緑（green.500）にする
  */
-export const TodoItem: VFC = memo(() => {
+export const TodoItem: VFC<Props> = memo((props) => {
+	const { onClickComplete, onClickDelete } = props;
+
 	return (
-		<ListItem p={4} borderBottom="1px solid" borderBottomColor="gray.100">
+		<ListItem p={3} borderBottom="1px solid" borderBottomColor="gray.100">
 			<Flex alignItems="center">
-				<Input value="TODOアイテム" border="none" isReadOnly />
 				<IconButton
 					aria-label="完了"
-					icon={<CheckCircleIcon />}
+					icon={<CheckIcon />}
 					variant="unstyled"
 					size="lg"
 					color="gray.100"
 					_hover={{ color: "green.500" }}
+					onClick={onClickComplete}
+				/>
+				<Box mx={1} flex={1}>
+					<Input value="TODOアイテム" variant="unstyle" isReadOnly />
+				</Box>
+				<IconButton
+					aria-label="完了"
+					icon={<CloseIcon />}
+					variant="unstyled"
+					size="md"
+					color="red.600"
+					onClick={onClickDelete}
 				/>
 			</Flex>
 		</ListItem>
