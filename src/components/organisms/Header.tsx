dc77a074282eab393a52ac9872c6d1592775d804
Chiaki Obara <obara@ham.works>
2021-05-13 18:34:57 +0900

    inputの入力実装

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,15 @@
-import React, { memo, VFC } from "react";
+import React, { memo, useState, VFC } from "react";
 import { Box, Flex, Heading, Input } from "@chakra-ui/react";
 
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Header: VFC = memo(() => {
+	const [todoText, setTodoText] = useState<string>("");
+
+	const onChangeTodoText = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setTodoText(event.target.value);
+	};
+
 	return (
 		<>
 			<Box bg="red.300" w="100%" textAlign="center" p={4} color="white">
@@ -13,7 +19,12 @@ export const Header: VFC = memo(() => {
 			</Box>
 			<Flex m={4}>
 				<Box flex="1">
-					<Input placeholder="TODOを入力" bg="white" />
+					<Input
+						placeholder="TODOを入力"
+						bg="white"
+						value={todoText}
+						onChange={onChangeTodoText}
+					/>
 				</Box>
 				<Box ml={2}>
 					<PrimaryButton>追加</PrimaryButton>
