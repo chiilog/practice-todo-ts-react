db2d2539059d2cc0b0652cb3aa35fd3a0145bb2b
Chiaki Obara <obara@ham.works>
2021-06-03 17:37:25 +0900

    todoを生成するtodoFactory作成(添削)

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,5 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
+
+type UUID = string;
 export type Todo = {
-  id: string;
+  id: UUID;
   todo: string | undefined;
   completed: boolean;
+};
+
+export const todoFactory = (todo: Partial<Todo>): Todo => {
+  return {
+    id: uuidv4(),
+    todo: "",
+    completed: false,
+    ...todo,
+  };
 };
